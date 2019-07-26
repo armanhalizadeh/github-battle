9cca9a942861cd6601ab20e2a1061823c8b6fafb
armanhalizadeh <armanhalizadeh@gamil.com>
2019-07-26 16:21:38 -0700

    Buttons control current language state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedLanguage: 'All'
+        };
+    }
+
+    navLanguageHandleClick(newLanguage) {
+        this.setState({
+            selectedLanguage: newLanguage
+        });
+    }
+
     render() {
         const languages =['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
         
@@ -9,7 +23,13 @@ export default class Popular extends React.Component {
                 {languages.map((lang) =>{
                     return(
                         <li key = {lang}>
-                            <button className= 'btn-clear nav-link'>
+                            <button 
+                                className= 'btn-clear nav-link'
+                                style = {lang === this.state.selectedLanguage 
+                                    ? {color: 'coral'}
+                                    : {color: 'black'}}
+                                onClick = {() => this.navLanguageHandleClick(lang)}
+                            >
                                 {lang}
                             </button>
                         </li>
