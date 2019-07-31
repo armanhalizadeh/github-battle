f64012b483771823de320e23d55dc19b4df9ba28
armanhalizadeh <armanhalizadeh@gmail.com>
2019-07-30 17:37:18 -0700

    Added and styled instructions to Battle Component

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
+import Battle from './components/Battle'
 
 // Component
 //  State
@@ -14,7 +15,7 @@ class App extends React.Component {
     render() {
         return (
             <div className = 'container'> 
-                <Popular />
+                <Battle />
             </div>
         )
     }
