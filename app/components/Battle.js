23b9cac52376cb3862feb5cf6e7fb7580cab6bc6
armanhalizadeh <armanhalizadeh@gmail.com>
2019-07-31 15:09:00 -0700

    Added player previews when username submitted

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 function Instructions () {
@@ -41,8 +41,7 @@ class PlayerInput extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-
-        this.props.onSubmit(this.state.userName);
+        this.props.onSubmit(this.state.username);
     }
 
     handleChange(event){
@@ -85,6 +84,38 @@ PlayerInput.propTypes = {
     label: PropTypes.string.isRequired,
 }
 
+function PlayerPreview({ username, onReset, label }) {
+    return (
+        <div className = 'column player'>
+            <h3 className = 'player-label'>{label}</h3>
+            <div className='bg-light row'>
+                <div className='player-info'>
+                    <img
+                        className='avatar-small'
+                        src={`https://github.com/${username}.png?size=200`}
+                        alt={`Avatar for ${username}`} 
+                    />
+                    <a
+                        href={`https://github.com/${username}`}
+                        className = 'link'
+                    >
+                        {username}
+                    </a>
+                </div>
+                <button className='btn-clear flex-center' onClick={onReset}>
+                    <FaTimesCircle color= 'rgb(194, 57, 42)' size= {26} />
+                </button>
+            </div>
+        </div>   
+    )
+}
+
+PlayerPreview.propTypes = {
+    username: PropTypes.string.isRequired,
+    onReset: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+}
+
 export default class Battle extends React.Component {
 constructor(props) {
     super(props)
@@ -95,11 +126,18 @@ constructor(props) {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
 }
 
 handleSubmit(id, player) {
     this.setState({
         [id]: player,
+    })
+}
+
+handleReset(id) {
+    this.setState({
+        [id]: null,
     })
 }
 
@@ -112,19 +150,29 @@ handleSubmit(id, player) {
                 <div className='players-container'>
                     <h1 className='center-text'>Players</h1>
                     <div className='row space-around'>
-                        {playerOne === null && (
-                            <PlayerInput 
+                        {playerOne === null
+                            ? <PlayerInput 
                                 label = 'Player One'
                                 onSubmit = {(player) => this.handleSubmit('playerOne', player)}
                             />
-                        )}
+                            : <PlayerPreview 
+                                username = {playerOne}
+                                label = 'Player One' 
+                                onReset = {()=> this.handleReset('playerOne')}
+                            />
+                        }
 
-                        {playerTwo === null && (
-                            <PlayerInput 
+                        {playerTwo === null
+                            ? <PlayerInput 
                                 label = 'Player Two'
                                 onSubmit = {(player) => this.handleSubmit('playerTwo', player)}
                             />
-                        )}
+                            : <PlayerPreview 
+                                username={playerTwo} 
+                                label='Player Two' 
+                                onReset={()=> this.handleReset('playerTwo')}
+                            />
+                        }
                     </div>
                 </div>
 
