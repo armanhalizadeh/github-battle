0e188042cf0df696669ecbe5afbde87f356100d2
armanhalizadeh <armanhalizadeh@gmail.com>
2019-08-12 16:29:22 -0700

    Added Tooltip component that shows info on hover

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -4,6 +4,7 @@ import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 import Card from './Card'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 //functional compononent that renders the navbar
 function LanguagesNav ({ selected, onUpdateLanguage}) {
@@ -53,10 +54,12 @@ function ReposGrid({ repos }) {
                         >
                             <ul className = 'card-list'>
                                 <li>
+                                    <Tooltip text = 'Github Username'>
                                     <FaUser color = 'rgb(255, 191, 116)' size={22}/>
                                     <a href={`https://github.com/${login}`}>
                                         {login}
                                     </a>
+                                    </Tooltip>
                                 </li>
                                 <li>
                                     <FaStar color = 'rgb(255, 215, 0)' size={22} />
--- app/components/Results.js
+++ app/components/Results.js
@@ -4,6 +4,7 @@ import {FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser} from 're
 import Card from './Card'
 import PropTypes from 'prop-types'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 //functional component that createst icons for results cards
 function ProfileList({profile}) {
@@ -15,14 +16,18 @@ function ProfileList({profile}) {
             </li>
             {profile.location && (
                 <li>
-                    <FaCompass color= 'rgb(144,115, 255)' size={22}/>
-                    {profile.location}
+                    <Tooltip text = "User's location">
+                        <FaCompass color= 'rgb(144,115, 255)' size={22}/>
+                        {profile.location}
+                    </Tooltip>
                 </li>
             )}
             {profile.company && (
                 <li>
-                    <FaBriefcase color= '#795548' size={22}/>
-                    {profile.company}
+                    <Tooltip text = "User's Company">
+                        <FaBriefcase color= '#795548' size={22}/>
+                        {profile.company}
+                    </Tooltip>
                 </li>
             )}
             <li>
